d7c5f57d1eaf3423c4d7655c592849d7e63d5e13
Martin Palmer <martin.palmer@almmechanics.com>
2019-12-15 21:51:37 +0000

    updated interface for terraform 12

--- tests/var.tf
+++ tests/var.tf
@@ -1,19 +1,19 @@
 variable "location" {
   description = "Azure region to be deployed into."
-  type        = "string"
+  type        = string
 }
 
 variable "id" {
   description = "Unique ID for this configuration."
-  type        = "string"
+  type        = string
 }
 
 variable "environment" {
   description = "Environment name."
-  type = "string"
+  type = string
 }
 
 variable "usage" {
   description = "Human friendly tag for this use of this environment."
-  type = "string"
+  type = string
 }
