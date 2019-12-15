b75482a4283062b9ec4106e698996a7b8e7ad6eb
Martin Palmer <martin.palmer@almmechanics.com>
2019-12-15 01:36:45 +0000

    Added resource group tests

---
+++ tests/ci/resourcegroup.tests.ps1
@@ -0,0 +1,24 @@
+param 
+(
+    [string]
+    $ResourceGroupName
+)
+
+Describe "Resource Group" {
+
+    $res = @(Get-AzureRmResource -ResourceGroupName $ResourceGroupName)
+    $resName = $res.name
+    Context "Resource Group common" {
+        It "Resource Group '$resName' exists" {
+            $res.count | should not benullorempty
+        }
+
+        It "Resource Group '$resName' located in uksouth" {
+            $res.Location | should be 'uksouth'
+        }
+
+        It "Only one resource exists in '$resName' " {
+            $res.Count | should be 1
+        }
+    }
+}
