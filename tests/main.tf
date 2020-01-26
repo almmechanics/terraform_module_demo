c28e68b214bb941e2eb1d6a9bf6e329b48d6d0fd
Martin Palmer <martin.palmer@almmechanics.com>
2020-01-26 20:37:28 +0000

    Only permit an azure backend

--- tests/main.tf
+++ tests/main.tf
@@ -1,9 +1,5 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "__terraformstorageaccount__"
-    container_name       = "terraform"
-    key                  = "terraform.tfstate"
-    access_key           = "__storagekey__"
   }
 }
 
